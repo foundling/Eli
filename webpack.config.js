449400779c368a5c46468a98119a494e12e4e775
Alex Ramsdell <aramsdell@artandlogic.com>
2022-07-13 17:26:39 -0400

    setup webpack config for live reload.

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/ts/app.ts',
+    mode: 'development',
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js']
+	},
+    devServer: {
+		static: path.join(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				exclude: /node_modules/,
+				use: [
+					{ loader: "ts-loader" }
+				]
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: [
+					"style-loader",
+					"css-loader"
+				]
+			},
+			{
+				test: /\.mp3$/,
+				exclude: /node_modules/,
+				use: [
+					{ loader: 'file-loader' }
+				]
+			}
+		]
+	},
+	plugins: [
+    new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+  ],
+}
