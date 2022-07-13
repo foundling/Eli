a455301b8325f3219364844e95216c7fb2a82b58
Alex Ramsdell <aramsdell@artandlogic.com>
2022-07-13 18:53:55 -0400

    very basic gameloop

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,1 +1,49 @@
-document.getElementById('box').textContent = 'live reload test';
+const FRAMERATE = 30; 
+
+let lastPressedTimestamp = 0;
+
+function update(): void {
+    //console.log('update')
+}
+function paint(): void {
+    //console.log('paint')
+}
+
+function step(timestamp: number): void {
+
+    const delta = timestamp - lastPressedTimestamp;
+
+    if (delta > FRAMERATE) {
+
+        lastPressedTimestamp = timestamp;
+        update();
+        paint();
+
+    }
+
+    requestAnimationFrame(step);
+
+}
+
+function onKeydown(e: KeyboardEvent) {
+    console.log('key press - ', e.key );
+}
+
+function bindEvents():void {
+
+    document.addEventListener('keydown', onKeydown);
+
+}
+
+function gameLoop(): void {
+
+    requestAnimationFrame(step);
+
+}
+
+function initGame() {
+    bindEvents();
+    gameLoop();
+}
+
+initGame();
